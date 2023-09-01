56fad3358b1065d2113450f1e18fbd4536bff5bb
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-09-01 21:32:38 +0100

    Setting up project

---
+++ database-python.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="report_email",
+    user="fionawiggins",
+    password="Skint962",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM report_data")
+
+print(cursor.fetchone())
